227731948635d362cf3aa3d58051c91aa66606aa
Arya Irani <538571+aryairani@users.noreply.github.com>
2021-06-08 21:12:21 +0000

    ucm-codebase-ui: add 1.0.M2f bottle.

--- Formula/ucm-codebase-ui.rb
+++ Formula/ucm-codebase-ui.rb
@@ -12,6 +12,13 @@ class UcmCodebaseUi < Formula
 
   head "https://github.com/unisonweb/codebase-ui.git"
 
+  bottle do
+    root_url "https://github.com/unisonweb/homebrew-unison-debug/releases/download/ucm-codebase-ui-1.0.M2f"
+    sha256 cellar: :any_skip_relocation, big_sur:      "66b824d36494e529514d7087e18dd2732307a5054e9674908c1234980261f814"
+    sha256 cellar: :any_skip_relocation, catalina:     "66b824d36494e529514d7087e18dd2732307a5054e9674908c1234980261f814"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "b1adcf677d107c589c4aa4ff75ee59fe9dfd51ecf60d5d61c996d8bbbd9de593"
+  end
+
   depends_on "npm" => :build
 
   def install
