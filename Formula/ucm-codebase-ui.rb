41479ed0c004bfeafa518b3d00a20bae0b1d73a4
Arya Irani <arya.irani@gmail.com>
2021-06-09 12:20:39 -0600

    update to release/M2m

--- Formula/ucm-codebase-ui.rb
+++ Formula/ucm-codebase-ui.rb
@@ -4,10 +4,9 @@ class UcmCodebaseUi < Formula
   desc "Unison Codebase UI"
   homepage "https://unisonweb.org"
 
-  url "https://github.com/unisonweb/codebase-ui.git",
-    revision: "8f08e8a2722066c961cbede520992c3a6011469f"
-  version "1.0.M2f"
-  sha256 "eed15ed8e5fd55511589bfacb5b0e83f6dcbc33b97730748530ce79b6e05fcd0"
+  url "https://github.com/unisonweb/codebase-ui.git", tag: "release/M2g"
+  version "1.0.M2g"
+  sha256 "b461e47c860a1e5c376aad40370f6ee0d1c1a658dbfa359c1056cbfd957d18a5"
   license "MIT"
 
   head "https://github.com/unisonweb/codebase-ui.git"
--- Formula/ucm.rb
+++ Formula/ucm.rb
@@ -2,19 +2,12 @@ class Ucm < Formula
   desc "Unison Codebase Manager"
   homepage "https://unisonweb.org"
 
-  url "https://github.com/unisonweb/unison/archive/refs/tags/release/M2f.tar.gz"
-  version "1.0.M2f"
-  sha256 "c566e7b549a264a8791bd3911e8e2ba2d29de471c056b6678f566c7b3fe8a457"
+  url "https://github.com/unisonweb/unison/archive/refs/tags/release/M2g.tar.gz"
+  version "1.0.M2g"
+  sha256 "a4abc27a02aa1bb3552c57079403d6d6c87cddc70767d40be002c7bb61b9b706"
   license "MIT"
 
   head "https://github.com/unisonweb/unison.git"
-
-  bottle do
-    root_url "https://github.com/unisonweb/homebrew-unison-debug/releases/download/ucm-1.0.M2f"
-    sha256 cellar: :any_skip_relocation, big_sur:      "75c9998f0d3e4d2687b7dfed927b3aab7f1754279bbd118e7d55de7a2a67e7be"
-    sha256 cellar: :any_skip_relocation, catalina:     "387a78cae5fdefc1f7478312cc362103b43c20a855d310849a67c2927ecb9f39"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "b5e2e506e9138fdd4cc5ef9e9d4ba062315af7865e6e265370fc28e314ff83ec"
-  end
 
   depends_on "ghc@8.10" => :build
   depends_on "haskell-stack" => :build
